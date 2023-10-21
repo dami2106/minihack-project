e0ab1a35c2ad49d2ab70b39c84d470dbea5ede30
Dami <damion@spiti.co.za>
2023-10-21 17:39:51 +0200

    Update a2c
    TODO fix A2C file

--- train_a2c.py
+++ train_a2c.py
@@ -11,19 +11,36 @@ from minihack import RewardManager
 import torch
 
 
-
 from a2c.agent import ACAgent
 from a2c.model import AdvantageActorCritic
 from a2c.helper import get_observation, normalize_glyphs, normalize_messages, make_video
 
+from environment_manager import setup_environment
+
 device = torch.device("cuda")
 
 
-env = gym.make(
-    "MiniHack-Room-5x5-v0",
-    observation_keys = ['pixel', 'message', 'glyphs'],
-)
+hyper_params = {
+        'replay-buffer-size': int(5e6),
+        'learning-rate': 1e-4,
+        'discount-factor': 0.99,  # discount factor
+        'num-steps': int(30000),  # Steps to run for, max episodes should be hit before this
+        'batch-size': 32,  
+        'learning-starts': 500,  # set learning to start after 1000 steps of exploration
+        'learning-freq': 1,  # Optimize after each step
+        'use-double-dqn': True,
+        'target-update-freq': 100, # number of iterations between every target network update
+        'eps-start': 1.0,  # e-greedy start threshold 
+        'eps-end': 0.1,  # e-greedy end threshold 
+        'eps-fraction': 0.7,  # Percentage of the time that epsilon is annealed
+        'print-freq': 10,
+        'seed' : 102,
+        'env' : "MiniHack-Room-5x5-v0",
+        'extra-info' : "config"  #config or plain
+    }
 
+env = setup_environment(hyper_params["env"], hyper_params["extra-info"])
+env.seed(hyper_params["seed"])  
 
 ac = ACAgent(
     observation_space = env.observation_space,
